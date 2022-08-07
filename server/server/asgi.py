320b1e9b7ae1b6bc316b5e9abc2f679303e59204
Paul Fleischer <fleischer89@gmail.com>
2022-08-07 10:45:24 +0000

    - Rearranging code order

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -11,11 +11,13 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import apps.chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-
 django.setup()
+
+import apps.chat.routing
+
+
 django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
   "http": django_asgi_app,
