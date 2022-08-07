3170bf0a640be467dc7de14f6dff7b73409d1cde
Paul Fleischer <fleischer89@gmail.com>
2022-08-07 10:35:20 +0000

    - Fixing bug in configuring ASGI

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -11,11 +11,14 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import apps.chat.routing
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
+django.setup()
+django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": django_asgi_app,
   "websocket": AuthMiddlewareStack(
         URLRouter(
             apps.chat.routing.websocket_urlpatterns
