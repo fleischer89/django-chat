5e8b0a9011977a99bff888cfca6ecebf667dc4d3
Paul Fleischer <fleischer89@gmail.com>
2022-08-08 13:58:14 +0000

    - Fixing STATICFILES_DIRS

--- server/server/settings.py
+++ server/server/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
