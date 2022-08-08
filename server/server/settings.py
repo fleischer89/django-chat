ce0c2486bb87e22ad252e78e2f2eb1272e0c560c
Paul Fleischer <fleischer89@gmail.com>
2022-08-08 13:59:25 +0000

    - Fixing STATIC_ROOT

--- server/server/settings.py
+++ server/server/settings.py
@@ -144,7 +144,7 @@ AUTH_USER_MODEL = 'user.User'
 STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
